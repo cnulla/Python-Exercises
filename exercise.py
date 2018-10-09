4c612f4022758ce0366ff12f99628bc3a71bdbee
camille <camillenulla28@gmail.com>
2018-10-09 13:45:02 +0800

    Number 2 Answer

--- exercise.py
+++ exercise.py
@@ -1,1 +1,18 @@
-print 'Hello Swiftkind'
+"""def list_of_persons(person):
+    person_add = {}
+    for every in person:
+        person_add.append(every)
+    return person_add
+
+i = raw_input("Enter persons: ")
+print list_of_persons(i)"""
+
+def insert_data(num):
+    new_list = []
+    for every in num:
+        new_list.insert(num)
+    return new_list
+
+
+
+
